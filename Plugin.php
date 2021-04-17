7efdb7106ef6cfbb249d4f2b827485957051be2e
flynsarmy <flynsarmy@gmail.com>
2021-04-17 16:18:58 +1000

    Render content block content

--- Plugin.php
+++ Plugin.php
@@ -3,10 +3,15 @@
 namespace Flynsarmy\ContentBlocks;
 
 use Backend\Widgets\Form;
-use Event;
-use System\Classes\PluginBase;
+use Cms\Classes\CmsCompoundObject;
 use Cms\Classes\Content as CmsContent;
 use Cms\Classes\Theme as CmsTheme;
+use Cms\Classes\Layout as CmsLayout;
+use Cms\Classes\Page as CmsPage;
+use Cms\Classes\Partial as CmsPartial;
+use Cms\Controllers\Index as CmsController;
+use Event;
+use System\Classes\PluginBase;
 
 class Plugin extends PluginBase
 {
@@ -29,20 +34,28 @@ class Plugin extends PluginBase
     public function boot()
     {
         /*
-         * Register menu items for the Winter.Pages plugin
+         * Update content blocks
+         */
+        Event::listen('cms.template.processSettingsBeforeSave', function (CmsController $controller, object &$dataHolder) {
+            // Unset the content block data so it doesn't save to the model
+            if (isset($dataHolder->settings['content_blocks'])) {
+                unset($dataHolder->settings['content_blocks']);
+            }
+        });
+
+        /*
+         * Add content blocks to the 'Content' tab
          */
         Event::listen('backend.form.extendFields', function (Form $widget) {
-            if (!$widget->getController() instanceof \CMS\Controllers\Index) {
+            if (!$widget->getController() instanceof CmsController) {
                 return;
             }
 
-            if (!$widget->model instanceof \Cms\Classes\Layout &&
-                !$widget->model instanceof \Cms\Classes\Page &&
-                !$widget->model instanceof \Cms\Classes\Partial) {
+            if (!$widget->model instanceof CmsLayout &&
+                !$widget->model instanceof CmsPage &&
+                !$widget->model instanceof CmsPartial) {
                 return;
             }
-
-            $theme = CmsTheme::getActiveTheme();
 
             // Current layout/page/partial markup
             $markup = $widget->model->getTwigContent();
@@ -56,31 +69,52 @@ class Plugin extends PluginBase
             $fields = [];
             foreach ($blocks[0] as $i => $block) {
                 $blockName = $blocks['name'][$i];
+
                 // Add the field to the form
                 $fields["settings[content_blocks][$blockName]"] = [
                     'label' => $blocks['name'][$i],
-                    'type' => $this->getBlockFieldType($blockName),
+                    'type' => $this->getFormFieldTypeForBlock($blockName),
+                    'comment' => $this->getFormFieldCommentForBlock($block),
                     'tab' => 'Content',
                 ];
 
-                // Get the HTML of the content block
-                $content = CmsContent::load($theme, $blocks['name'][$i]);
-                if ($content) {
-                    $content = json_decode($content, true);
-                    if ($content) {
-                        $content = $content['content'];
-                    }
-                }
-
-                // Add the data to the model
-                $widget->model->settings['content_blocks'][$blockName] = $content;
+                // Add the content block to the model
+                $widget->model->settings['content_blocks'][$blockName] =
+                    $this->getContentBlock($blockName);
             }
 
             $widget->addFields($fields, 'primary');
         });
     }
 
-    public function getBlockFieldType(string $filename): string
+    /**
+     * Returns the contents of the content block with given filename or empty
+     * string if the content block doesn't exist.
+     *
+     * @param string $filename
+     * @return string
+     */
+    public function getContentBlock(string $filename): string
+    {
+        $content = json_decode(
+            CmsContent::load(CmsTheme::getActiveTheme(), $filename),
+            true
+        );
+
+        if (!$content) {
+            return '';
+        }
+
+        return $content['content'];
+    }
+
+    /**
+     * Returns the form field type for given block.
+     *
+     * @param string $filename
+     * @return string
+     */
+    public function getFormFieldTypeForBlock(string $filename): string
     {
         $ext = strtolower(pathinfo($filename, PATHINFO_EXTENSION));
 
@@ -92,5 +126,18 @@ class Plugin extends PluginBase
             default:
                 return 'richeditor';
         }
+    }
+
+    public function getFormFieldCommentForBlock(string $block): string
+    {
+        // Grab all argument names passed to this block
+        preg_match_all("/\s(?P<args>[a-zA-Z]+)=/", $block, $args);
+        $args = $args['args'];
+
+        if (!count($args)) {
+            return "";
+        }
+
+        return "This block may use the following variables: {{ " . implode(' }}, {{', $args) . ' }}';
     }
 }
